85248eebae5d90faebeaff922ac6c6ec0dfbceea
Pushparaj Jayaseelan <pushparaj.jayaseelan>
2018-08-20 11:00:35 +0530

    iPlayerView added.

---
+++ Example/iPlayer/VideoPlayerVC.swift
@@ -0,0 +1,35 @@
+//
+//  VideoPlayerVC.swift
+//  iPlayer_Example
+//
+//  Created by Pushparaj Jayaseelan on 20/08/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class VideoPlayerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iPlayer/Classes/IPlayerView.swift
@@ -0,0 +1,20 @@
+//
+//  IPlayerView.swift
+//  iPlayer
+//
+//  Created by Pushparaj Jayaseelan on 20/08/18.
+//
+
+import UIKit
+
+class IPlayerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
